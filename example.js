73ae4842230ea2b53771be915b87edb1b6a47d1d
Erwan d'Orgeville <info@erwandorgeville.com>
2016-04-22 16:14:17 -0400

    Fix example

--- example.js
+++ example.js
@@ -3,7 +3,7 @@ fbLikeCount.client_id = process.env.FB_APP_ID || '123456789000000';
 fbLikeCount.client_secret = process.env.FB_APP_SECRET || '123456789012345678901234567890ee';
 // fbLikeCount.debug = true;
 
-setTimeout(function(){
+setInterval(function(){
   fbLikeCount.get('FacebookforDevelopers', function(count){
     console.log(count);
   });
